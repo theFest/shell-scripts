e26cba95d02fbd56c7d6891aecbbaee546abb9fc
theFest <8981383+theFest@users.noreply.github.com>
2023-09-13 01:24:33 +0200

    Update regional_setting
    minor fixes with added features

--- debian/regional_setting.sh
+++ debian/regional_setting.sh
@@ -6,11 +6,102 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-# Main menu using Zenity
+# Function to set locale
+set_locale() {
+    LOCALES=$(locale -a)
+    LOCALE=$(zenity --list \
+        --title "Set Locale" \
+        --text "Select a locale:" \
+        --column "Locale" --column "Description" \
+        $(for locale in $LOCALES; do echo "$locale" "$locale"; done))
+
+    if [ -n "$LOCALE" ]; then
+        locale-gen "$LOCALE"
+        update-locale LANG="$LOCALE"
+        zenity --info --text "Locale set to $LOCALE. Please restart your session to apply changes."
+    else
+        zenity --error --text "Invalid locale. Please try again."
+    fi
+}
+
+# Function to set keyboard layout
+set_keyboard_layout() {
+    KEYBOARD_LAYOUT=$(zenity --list \
+        --title "Set Keyboard Layout" \
+        --text "Select a keyboard layout:" \
+        --column "Layout" --column "Description" \
+        "us" "US English" \
+        "gb" "British English" \
+        "fr" "French" \
+        "de" "German" \
+        "es" "Spanish" \
+        "it" "Italian" \
+        "ru" "Russian")
+
+    if [ -n "$KEYBOARD_LAYOUT" ]; then
+        setxkbmap "$KEYBOARD_LAYOUT"
+        zenity --info --text "Keyboard layout set to $KEYBOARD_LAYOUT."
+    else
+        zenity --error --text "Invalid keyboard layout. Please try again."
+    fi
+}
+
+# Function to set timezone
+set_timezone() {
+    TIMEZONE=$(zenity --entry --title "Set Timezone" --text "Enter the desired timezone (e.g., America/New_York):")
+    if [ -n "$TIMEZONE" ]; then
+        timedatectl set-timezone "$TIMEZONE"
+        zenity --info --text "Timezone set to $TIMEZONE."
+    else
+        zenity --error --text "Invalid timezone. Please try again."
+    fi
+}
+
+# Function to set date format
+set_date_format() {
+    DATE_FORMAT=$(zenity --list \
+        --title "Set Date Format" \
+        --text "Select a date format:" \
+        --column "Format" --column "Description" \
+        "+%Y-%m-%d" "YYYY-MM-DD" \
+        "+%d-%m-%Y" "DD-MM-YYYY" \
+        "+%m-%d-%Y" "MM-DD-YYYY")
+
+    if [ -n "$DATE_FORMAT" ]; then
+        timedatectl set-local-rtc 0
+        timedatectl set-time-format "$DATE_FORMAT"
+        zenity --info --text "Date format set to $DATE_FORMAT."
+    else
+        zenity --error --text "Invalid date format. Please try again."
+    fi
+}
+
+# Function to set language preferences
+set_language_preferences() {
+    LANGUAGES=$(zenity --list \
+        --title "Set Language Preferences" \
+        --text "Select language preferences (comma-separated, e.g., en_US,fr_FR,es_ES):" \
+        --column "Language" --column "Description" \
+        "en_US" "English (US)" \
+        "fr_FR" "French (France)" \
+        "es_ES" "Spanish (Spain)" \
+        "de_DE" "German (Germany)" \
+        "it_IT" "Italian (Italy)" \
+        "ru_RU" "Russian (Russia)")
+
+    if [ -n "$LANGUAGES" ]; then
+        localectl set-locale LANG="$LANGUAGES"
+        zenity --info --text "Language preferences set to $LANGUAGES. Please restart your session to apply changes."
+    else
+        zenity --error --text "Invalid language preferences. Please try again."
+    fi
+}
+
+# Main menu using Zenity with improved styling and larger windows
 CHOICE=$(zenity --list \
     --title "Regional Settings Management" \
     --text "Select an option:" \
-    --column "Option" --column "Description" \
+    --column "Option" --column "Description" --width 400 --height 350 \
     "1" "Set Locale" \
     "2" "Set Keyboard Layout" \
     "3" "Set Timezone" \
@@ -20,91 +111,12 @@ CHOICE=$(zenity --list \
 
 # Perform actions based on user's choice
 case $CHOICE in
-    "1") # Set Locale
-        LOCALES=$(locale -a)
-        LOCALE=$(zenity --list \
-            --title "Set Locale" \
-            --text "Select a locale:" \
-            --column "Locale" --column "Description" \
-            $(for locale in $LOCALES; do echo "$locale" "$locale"; done))
-
-        if [ -n "$LOCALE" ]; then
-            locale-gen "$LOCALE"
-            update-locale LANG="$LOCALE"
-            zenity --info --text "Locale set to $LOCALE. Please restart your session to apply changes."
-        else
-            zenity --error --text "Invalid locale. Please try again."
-        fi
-        ;;
-    "2") # Set Keyboard Layout
-        KEYBOARD_LAYOUT=$(zenity --list \
-            --title "Set Keyboard Layout" \
-            --text "Select a keyboard layout:" \
-            --column "Layout" --column "Description" \
-            "us" "US English" \
-            "gb" "British English" \
-            "fr" "French" \
-            "de" "German" \
-            "es" "Spanish" \
-            "it" "Italian" \
-            "ru" "Russian")
-
-        if [ -n "$KEYBOARD_LAYOUT" ]; then
-            setxkbmap "$KEYBOARD_LAYOUT"
-            zenity --info --text "Keyboard layout set to $KEYBOARD_LAYOUT."
-        else
-            zenity --error --text "Invalid keyboard layout. Please try again."
-        fi
-        ;;
-    "3") # Set Timezone
-        TIMEZONE=$(zenity --entry --title "Set Timezone" --text "Enter the desired timezone (e.g., America/New_York):")
-        if [ -n "$TIMEZONE" ]; then
-            timedatectl set-timezone "$TIMEZONE"
-            zenity --info --text "Timezone set to $TIMEZONE."
-        else
-            zenity --error --text "Invalid timezone. Please try again."
-        fi
-        ;;
-    "4") # Set Date Format
-        DATE_FORMAT=$(zenity --list \
-            --title "Set Date Format" \
-            --text "Select a date format:" \
-            --column "Format" --column "Description" \
-            "+%Y-%m-%d" "YYYY-MM-DD" \
-            "+%d-%m-%Y" "DD-MM-YYYY" \
-            "+%m-%d-%Y" "MM-DD-YYYY")
-
-        if [ -n "$DATE_FORMAT" ]; then
-            timedatectl set-local-rtc 0
-            timedatectl set-time-format "$DATE_FORMAT"
-            zenity --info --text "Date format set to $DATE_FORMAT."
-        else
-            zenity --error --text "Invalid date format. Please try again."
-        fi
-        ;;
-    "5") # Set Language Preferences
-        LANGUAGES=$(zenity --list \
-            --title "Set Language Preferences" \
-            --text "Select language preferences (comma-separated, e.g., en_US,fr_FR,es_ES):" \
-            --column "Language" --column "Description" \
-            "en_US" "English (US)" \
-            "fr_FR" "French (France)" \
-            "es_ES" "Spanish (Spain)" \
-            "de_DE" "German (Germany)" \
-            "it_IT" "Italian (Italy)" \
-            "ru_RU" "Russian (Russia)")
-
-        if [ -n "$LANGUAGES" ]; then
-            localectl set-locale LANG="$LANGUAGES"
-            zenity --info --text "Language preferences set to $LANGUAGES. Please restart your session to apply changes."
-        else
-            zenity --error --text "Invalid language preferences. Please try again."
-        fi
-        ;;
-    *) # Exit or cancel
-        zenity --info --text "Exiting Regional Settings Management."
-        exit 0
-        ;;
+    "1") set_locale;;
+    "2") set_keyboard_layout;;
+    "3") set_timezone;;
+    "4") set_date_format;;
+    "5") set_language_preferences;;
+    *)   zenity --info --text "Exiting Regional Settings Management.";;
 esac
 
 exit 0
