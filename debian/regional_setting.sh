d55e66088b9b63476232e5dccaa7b50a226fa742
theFest <8981383+theFest@users.noreply.github.com>
2023-09-18 10:50:11 +0200

    Update regional_setting
    added options

--- debian/regional_setting.sh
+++ debian/regional_setting.sh
@@ -10,7 +10,7 @@ fi
 CHOICE=$(zenity --list \
     --title "Regional Settings Management" \
     --width=400 \
-    --height=300 \
+    --height=400 \
     --text "Select an option:" \
     --column "Option" --column "Description" \
     "1" "Set Locale" \
@@ -18,8 +18,10 @@ CHOICE=$(zenity --list \
     "3" "Set Timezone" \
     "4" "Set Date Format" \
     "5" "Set Language Preferences" \
-    "6" "View Current Settings" \
-    "7" "Exit")
+    "6" "Set Hostname" \
+    "7" "Set System Clock" \
+    "8" "View Current Settings" \
+    "9" "Exit")
 
 # Perform actions based on user's choice
 case $CHOICE in
@@ -112,8 +114,44 @@ case $CHOICE in
             zenity --error --width=400 --text "Invalid language preferences. Please try again."
         fi
         ;;
-    "6") # View Current Settings
-        CURRENT_SETTINGS=$(zenity --info --title "Current Regional Settings" --width=600 --height=400 --text "Current Locale: $(locale)\nCurrent Keyboard Layout: $(setxkbmap -query | grep layout | awk '{print $2}')\nCurrent Timezone: $(timedatectl show --property=Timezone --value)\nCurrent Date Format: $(timedatectl show --property=TimeFormat --value)\nCurrent Language Preferences: $(localectl list-locales)")
+    "6") # Set Hostname
+        NEW_HOSTNAME=$(zenity --entry --title "Set Hostname" --text "Enter the desired hostname:")
+        if [ -n "$NEW_HOSTNAME" ]; then
+            hostnamectl set-hostname "$NEW_HOSTNAME"
+            zenity --info --width=400 --text "Hostname set to $NEW_HOSTNAME. Please restart your system to apply changes."
+        else
+            zenity --error --width=400 --text "Invalid hostname. Please try again."
+        fi
+        ;;
+    "7") # Set System Clock
+        CLOCK_OPTIONS=$(zenity --list \
+            --title "Set System Clock" \
+            --width=400 \
+            --height=400 \
+            --text "Select system clock options:" \
+            --column "Option" --column "Description" \
+            "1" "Synchronize with NTP Server" \
+            "2" "Manual Time and Date Setting")
+
+        case $CLOCK_OPTIONS in
+            "1")
+                timedatectl set-ntp yes
+                zenity --info --width=400 --text "System clock synchronized with NTP server."
+                ;;
+            "2")
+                DATE_TIME=$(zenity --calendar --title "Set Date and Time" --text "Select a date and time:")
+                if [ -n "$DATE_TIME" ]; then
+                    timedatectl set-time "$DATE_TIME"
+                    zenity --info --width=400 --text "System date and time set to $DATE_TIME."
+                else
+                    zenity --error --width=400 --text "Invalid date and time. Please try again."
+                fi
+                ;;
+            *) ;;
+        esac
+        ;;
+    "8") # View Current Settings
+        CURRENT_SETTINGS=$(zenity --info --title "Current Regional Settings" --width=600 --height=400 --text "Current Locale: $(locale)\nCurrent Keyboard Layout: $(setxkbmap -query | grep layout | awk '{print $2}')\nCurrent Timezone: $(timedatectl show --property=Timezone --value)\nCurrent Date Format: $(timedatectl show --property=TimeFormat --value)\nCurrent Language Preferences: $(localectl list-locales)\nCurrent Hostname: $(hostname)\nCurrent System Clock Status: $(timedatectl show --property=NTP --value)")
 
         ;;
     *) # Exit or cancel
